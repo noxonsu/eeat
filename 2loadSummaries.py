900dbd7a7c02390812906b6f098d635c3ce8934b
marsiandeployer <129854151+marsiandeployer@users.noreply.github.com>
2023-10-26 16:26:13 +0000

    fix few

--- 2loadSummaries.py
+++ 2loadSummaries.py
@@ -32,10 +32,10 @@ def extract_content(site):
     return text_content[0].page_content
 
 
-def is_product_or_list(summary):
+def is_product_or_list(summary,company_products):
     chat = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k-0613")
     messages = [
-        SystemMessage(content="Determine if the given summary describes a few products of one company or a few products from different companies. If it's a list of products from different companies, extract Names mentioned in the summary in format 'Company_name: product_name' or just 'product_name'. Return a 'list of products: 1.' with number near each product and with the company name near every product's name. In case This summary does not describe a single product or a list of products from different companies return 'Invalid'"),
+        SystemMessage(content="We are looking for KYC providers SaaS or API. Determine if the provided summary describes either multiple products (services, projects) from different companies or a few products from the same company. If it's a list of products from different companies, extract the names mentioned in the summary in the format 'Company_name: product_name' or just 'product_name'. Then, return a 'list of products' with each product numbered and the company name (if applicable) mentioned next to each product's name, with one product per line. If the summary does not describe a single product or a list of products from different companies, return 'Invalid' and provide a brief explanation as to why."),
         HumanMessage(content=summary)
     ]
 
@@ -44,20 +44,17 @@ def is_product_or_list(summary):
         gpt_response = response.content
 
         # Check if it's a list of products
-        if "nvalid" in gpt_response:
+        if "invalid" in gpt_response:
             return "invalid", []
         elif "List of products" in gpt_response:
             # Extract company-product pairs from the response
             product_lines = gpt_response.split("\n")
-            company_products = load_products("products.json")
 
             for line in product_lines:
-                if line.strip().startswith(str(len(company_products) + 1) + ". "):
-                    # Extract the company and product name from each line
-                    parts = line.strip().split(":")
-                    if len(parts) >= 2:
-                        company_name, product_name = parts[0], parts[1]
-                        company_products.append(f"{company_name}: {product_name}")
+                parts = line.strip().split(":")
+                if len(parts) >= 2:
+                    company_name, product_name = parts[0], parts[1]
+                    company_products.append(f"{company_name}: {product_name}")
 
             return "list of products", company_products
         else:
@@ -66,6 +63,7 @@ def is_product_or_list(summary):
     except Exception as e:
         print(f"An error occurred: {e}")
         return "unknown", []
+
 
 
 def extract_links_from_summary(summary):
@@ -79,7 +77,7 @@ def main():
     data = load_data_without_nature("data.json")
 
     # Initialize company_products as an empty list
-    company_products = []
+    
 
     try:
         # Load company_products from "products.json" if it exists
@@ -90,10 +88,11 @@ def main():
 
     # Iterate through the data dictionary
     for domain, domain_data in data.items():
+        company_products = load_products("products.json")
         url = domain_data["url"]
         summary = extract_content(url)
 
-        nature, extracted_links = is_product_or_list(summary)
+        nature, extracted_links = is_product_or_list(summary,company_products)
 
         # Save the nature into the data dictionary for that domain
         data[domain]["nature"] = nature
