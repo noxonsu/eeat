6fb105c9952877ca673e421eeda26b005bf8b59a
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-12-05 13:26:25 +0000

    medium ready

--- publish_to_medium.py
+++ publish_to_medium.py
@@ -1,9 +1,30 @@
 import os
 import requests
 
-def publish_to_medium(title, content, tags, integration_token):
+def get_user_id(integration_token):
+    # URL to get the authenticated user's information
+    url = 'https://api.medium.com/v1/me'
+
+    # Headers including the authentication token
+    headers = {
+        'Authorization': f'Bearer {integration_token}',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8'
+    }
+
+    # Sending GET request to Medium
+    response = requests.get(url, headers=headers)
+
+    # Handling the response
+    if response.ok:
+        return response.json()['data']['id']
+    else:
+        raise Exception("Failed to retrieve user ID: " + response.text)
+
+def publish_to_medium(author_id, title, content, tags, integration_token):
     # Medium API URL for creating posts
-    api_url = 'https://api.medium.com/v1/me/posts'
+    api_url = f'https://api.medium.com/v1/users/{author_id}/posts'
 
     # Formatting the tags into a list
     tags_list = tags.split(",") if tags else []
@@ -13,8 +34,8 @@ def publish_to_medium(title, content, tags, integration_token):
         'title': title,
         'contentFormat': 'markdown',  # Assuming content is in markdown format
         'content': content,
-        'publishStatus': 'draft',  # Change to 'public' to publish immediately
         'tags': tags_list,
+        'publishStatus': 'public',  # Change to 'public' to publish immediately
     }
 
     # Headers including the authentication token
@@ -49,5 +70,8 @@ medium_title = os.environ['MEDIUM_TITLE']
 medium_text = os.environ['MEDIUM_TEXT']
 medium_tags = os.environ['MEDIUM_TAGS']
 
+# Getting the user's authorId
+author_id = get_user_id(integration_token)
+
 # Publishing to Medium
-publish_to_medium(medium_title, medium_text, medium_tags, integration_token)
+publish_to_medium(author_id, medium_title, medium_text, medium_tags, integration_token)
